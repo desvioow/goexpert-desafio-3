ebe2e7f1b9de0588bd84cf60ddf3538764e133a4
lucas <deliberato.lucas@gmail.com>
2024-09-08 13:38:43 -0300

    Add web server implementation
    Introduce a web server implementation with order handling capabilities. This includes a new web server starter, order handler, and necessary dependencies for routing and middleware.

---
+++ internal/infra/web/webserver/order_handler.go
@@ -0,0 +1,48 @@
+package webserver
+
+import (
+	"encoding/json"
+	"github.com/desvioow/goexpert-desafio-3/internal/entity"
+	"github.com/desvioow/goexpert-desafio-3/internal/usecase"
+	"github.com/desvioow/goexpert-desafio-3/pkg/events"
+	"net/http"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher   events.EventDispatcherInterface
+	OrderRepository   entity.OrderRepositoryInterface
+	OrderCreatedEvent events.EventInterface
+}
+
+func NewWebOrderHandler(
+	EventDispatcher events.EventDispatcherInterface,
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreatedEvent events.EventInterface,
+) *WebOrderHandler {
+	return &WebOrderHandler{
+		EventDispatcher:   EventDispatcher,
+		OrderRepository:   OrderRepository,
+		OrderCreatedEvent: OrderCreatedEvent,
+	}
+}
+
+func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var dto usecase.OrderInputDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createOrder := usecase.NewCreateOrderUseCase(h.OrderRepository, h.OrderCreatedEvent)
+	output, err := createOrder.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- internal/usecase/create_order.go
+++ internal/usecase/create_order.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"github.com/desvioow/goexpert-desafio-3/internal/entity"
+	"github.com/desvioow/goexpert-desafio-3/pkg/events"
 )
 
 type OrderInputDTO struct {
@@ -19,18 +20,42 @@ type OrderOutputDTO struct {
 
 type CreateOrderUseCase struct {
 	OrderRepository entity.OrderRepositoryInterface
-	//OrderCreated    events.EventInterface
+	OrderCreated    events.EventInterface
 	//EventDispatcher events.EventDispatcherInterface
 }
 
 func NewCreateOrderUseCase(
 	OrderRepository entity.OrderRepositoryInterface,
-	// OrderCreated events.EventInterface,
+	OrderCreated events.EventInterface,
 	// EventDispatcher events.EventDispatcherInterface,
 ) *CreateOrderUseCase {
 	return &CreateOrderUseCase{
 		OrderRepository: OrderRepository,
-		//OrderCreated:    OrderCreated,
+		OrderCreated:    OrderCreated,
 		//EventDispatcher: EventDispatcher,
 	}
+}
+
+func (c *CreateOrderUseCase) Execute(input OrderInputDTO) (OrderOutputDTO, error) {
+	order := entity.Order{
+		ID:    input.ID,
+		Price: input.Price,
+		Tax:   input.Tax,
+	}
+	order.CalculateFinalPrice()
+	if err := c.OrderRepository.Save(&order); err != nil {
+		return OrderOutputDTO{}, err
+	}
+
+	dto := OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.Price + order.Tax,
+	}
+
+	c.OrderCreated.SetPayload(dto)
+	//c.EventDispatcher.Dispatch(c.OrderCreated)
+
+	return dto, nil
 }
